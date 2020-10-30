ef0381b5a773e134fa90ffcd42dcc75012b519ac
liufei <liufei@dragonsoft.com.cn>
2020-10-30 15:55:02 +0800

    修改md5的bug

--- src/main/java/com/code/common/utils/MD5.java
+++ src/main/java/com/code/common/utils/MD5.java
@@ -28,7 +28,8 @@ public class MD5 {
         // 16进制数字
         String md5code = new BigInteger(1, secretBytes).toString(16);
         // 如果生成数字未满32位，需要前面补0
-        for (int i = 0; i < 32 - md5code.length(); i++) {
+        int codeLength =md5code.length();
+        for (int i = 0; i < 32 - codeLength; i++) {
             md5code = "0" + md5code;
         }
         return md5code;
